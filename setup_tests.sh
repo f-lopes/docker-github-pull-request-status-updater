f90b8fa3bf62170897b0ea860d4b3d3ca8edf09b
Florian Lopes <florian.lopes@outlook.com>
2018-07-06 07:20:42 +0200

    Update Travis CI build: rename container-structure-test binary

--- setup_tests.sh
+++ setup_tests.sh
@@ -6,9 +6,9 @@ echo "Retrieving latest container-structure-test binary...."
 echo
 
 curl -LO https://storage.googleapis.com/container-structure-test/latest/container-structure-test \
-	&& mv container-structure-test structure-test \
-	&& sudo cp structure-test /usr/local/bin \
-	&& sudo chmod +x /usr/local/bin/structure-test
+	&& mv container-structure-test container-structure-test \
+	&& sudo cp container-structure-test /usr/local/bin \
+	&& sudo chmod +x /usr/local/bin/container-structure-test
 
 export PATH=$PATH:/usr/local/bin
 
